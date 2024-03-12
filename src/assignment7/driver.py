a376ad80a16866f556f8d0bd373f378c0675edcc
davidselvam <smilingdavid001@gmail.com>
2024-03-12 21:06:16 +0530

    Assignment 7

---
+++ src/assignment7/driver.py
@@ -0,0 +1,2 @@
+from src.assignment7.util import find_day_of_week
+print(find_day_of_week(12, 3, 2024))
---
+++ test/assignment7/test.py
@@ -0,0 +1,17 @@
+import unittest
+
+from src.assignment7.util import find_day_of_week
+class MyTestCase(unittest.TestCase):
+    def test_something1(self):
+        expected_output = "TUESDAY"
+        actual_output = find_day_of_week(3, 12, 2024)
+        self.assertEqual(expected_output,actual_output)  # first testcase
+
+    def test_something2(self):
+        expected_output = "MONDAY"
+        actual_output = find_day_of_week(11, 6, 2000)
+        self.assertEqual(expected_output,actual_output)  # second testcase
+
+
+if __name__ == '__main__':
+    unittest.main()
