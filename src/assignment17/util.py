7ef9e6659521b3aec7470e5db2f80ee730dafe34
davidselvam <smilingdavid001@gmail.com>
2024-03-13 13:43:45 +0530

    Assignment 17 - Iterables and Iterators

---
+++ src/assignment17/util.py
@@ -0,0 +1,15 @@
+# from itertools import combinations
+#
+# def calculate_probability(n, l, k):
+#     all_combinations = list(combinations(l, k))
+#     filter_combinations = filter(lambda c: 'a' in c, all_combinations)
+#     probability = len(list(all_combinations)) / len(filter_combinations)
+#     return "{0:.3f}".format(probability)
+
+from itertools import combinations
+
+def calculate_probability(n, l, k):
+    all_combinations = list(combinations(l, k))
+    filter_combinations = filter(lambda c: 'a' in c, all_combinations)
+    probability = len(list(filter_combinations)) / len(all_combinations)
+    return float("{0:.3f}".format(probability))
---
+++ test/assignment17/test.py
@@ -0,0 +1,16 @@
+import unittest
+
+from src.assignment17.util import calculate_probability
+class MyTestCase(unittest.TestCase):
+    def test_something1(self):
+        excepted_output = calculate_probability(4,"a a c d",2)
+        actual_output = 0.524
+        self.assertEqual(excepted_output,actual_output)  # first testcase
+    def test_something2(self):
+        excepted_output = calculate_probability(6,"aa bb cc aa dd ff",2)
+        actual_output = 0.426
+        self.assertEqual(excepted_output,actual_output)  # second testcase
+
+
+if __name__ == '__main__':
+    unittest.main()
