3299c7ce564cd41d3f2bd70e54e5a438ca9555dd
davidselvam <smilingdavid001@gmail.com>
2024-03-13 18:27:33 +0530

    Assignment 13 - Mean,Var and Std

---
+++ src/assignment13/driver.py
@@ -0,0 +1,3 @@
+from src.assignment13.util import calculate_statistics
+array = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+print(calculate_statistics(array))
---
+++ test/assignment13/test.py
@@ -0,0 +1,13 @@
+import unittest
+
+from src.assignment13.util import calculate_statistics
+class MyTestCase(unittest.TestCase):
+    def test_something1(self):
+        array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        excepted_output = calculate_statistics(array)
+        actual_output = "[2. 5. 8.]\n[6. 6. 6.]\n2.58198889747"
+        self.assertEqual(excepted_output,actual_output)  # first testcase
+
+
+if __name__ == '__main__':
+    unittest.main()
