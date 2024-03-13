c7c383313f201d35e273de384ba5bfcd912d825f
davidselvam <smilingdavid001@gmail.com>
2024-03-13 18:53:10 +0530

    Assignment 6 - Text Alignment

---
+++ src/assignment6/driver.py
@@ -0,0 +1,3 @@
+from src.assignment6.util import pattern
+thickness = int(input("Enter thickness an odd number: "))
+print(pattern(thickness))
---
+++ test/assignment6/test.py
@@ -0,0 +1,30 @@
+import unittest
+
+from src.assignment6.util import pattern
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        thickness = int(input("Enter thickness an odd number: "))
+        expected_output = pattern(thickness,c="H")
+        actual_output =  [
+            '  H  ',
+            ' HHH ',
+            'HHHHH',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            ' HHHHHHHHHHHHHHH  ',
+            ' HHHHHHHHHHHHHHH  ',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            ' HHH         HHH        ',
+            '            HHHHH ',
+            '             HHH  ',
+            '              H   ',
+        ]
+        self.assertEqual(expected_output,expected_output)  # testcase
+
+
+if __name__ == '__main__':
+    unittest.main()
