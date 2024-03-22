bd05238e259ae0cf67e60cd9fdc968bffa119063
davidselvam <smilingdavid001@gmail.com>
2024-03-22 14:12:36 +0530

    Added Linear Algebra problem and test the cases.

--- src/assignment12/util.py
+++ src/assignment12/util.py
@@ -3,3 +3,4 @@ def calculating_determinant(n, matrix_elements):
     matrix = np.array(matrix_elements, dtype=float).reshape(n, n)
     determinant_value = round(np.linalg.det(matrix), 2)
     return determinant_value
+
