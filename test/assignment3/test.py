32c6ea4c28fc66b7a86c30c84b3c95f6a9dbf996
davidselvam <smilingdavid001@gmail.com>
2024-03-22 13:40:27 +0530

    Solved Mutations problem and check the test cases.

--- test/assignment3/test.py
+++ test/assignment3/test.py
@@ -5,11 +5,11 @@ from src.assignment3.util import change_string
 class MyTestCase(unittest.TestCase):
     def test_something1(self):
         expected_output = change_string("ravid",0,"d")
-        self.assertEqual(expected_output,"david")  # 1st testcase
+        self.assertEqual(expected_output,"david")  # first testcase
 
     def test_something2(self):
         expected_output = change_string("vani", 0, "m")
-        self.assertEqual(expected_output, "mani")  # 2nd testcase
+        self.assertEqual(expected_output, "mani")  # second testcase
 
 
 if __name__ == '__main__':
