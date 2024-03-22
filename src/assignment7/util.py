57a67b880a9471b65c151ad473adba871a96c6d0
davidselvam <smilingdavid001@gmail.com>
2024-03-22 13:58:55 +0530

    Added Calendar Module problem and test the cases.

--- src/assignment7/util.py
+++ src/assignment7/util.py
@@ -3,3 +3,4 @@ def find_day_of_week(month, day, year):
     n = calendar.weekday(year, month, day)
     day_of_week = calendar.day_name[n]
     return day_of_week.upper()
+
--- test/assignment7/test.py
+++ test/assignment7/test.py
@@ -15,3 +15,4 @@ class MyTestCase(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
